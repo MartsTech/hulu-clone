25ac9cef8b286034ba285af60ad9e6762c96a4cf
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2021-04-27 13:05:56 +0300

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Hulu Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
